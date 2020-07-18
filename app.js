58d1cdb74e9752bfb9fc6b6177b9aeacb7a90798
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-07-18 13:57:42 +0200

    added scroll animation to sliders

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+// Scene
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
   // init Controller
@@ -8,7 +10,7 @@ function animateSlides() {
   const sliders = document.querySelectorAll(".slide");
   const nav = document.querySelectorAll(".nav-header");
   // loop over
-  sliders.forEach((slide) => {
+  sliders.forEach((slide, index, slides) => {
     const img = slide.querySelector("img");
     const revealImg = slide.querySelector(".reveal-img");
     const revealText = slide.querySelector(".reveal-text");
@@ -27,12 +29,34 @@ function animateSlides() {
       triggerHook: 0.25,
       reverse: false,
     })
-      .setTween(slideTl)
       .addIndicators({
         colorStart: "white",
         colorTrigger: "white",
         name: "slide",
       })
+      .setTween(slideTl)
+      .addTo(controller);
+    // new animation
+    const pageTl = gsap.timeline();
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    pageTl.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+    pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+    pageTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+    // Create ne scene
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "page",
+        indent: 200,
+      })
+      // to make the slide section sticky - pushFollowers to avoid hugh section
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageTl)
       .addTo(controller);
   });
 }
