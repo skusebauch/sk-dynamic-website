b5094d6675796e03f7a11192fa4f3cb178a15e9b
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-07-18 23:07:54 +0200

    add some mq

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ function animateSlides() {
     //animate 1s sooner as defaults "-=1"
     slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
     slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
-    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.75");
     // Create Scene
     slideScene = new ScrollMagic.Scene({
       triggerElement: slide,
@@ -137,12 +136,6 @@ barba.init({
       beforeEnter() {
         logo.href = "../index.html";
         fashionAnimation();
-        gsap.fromTo(
-          ".nav-header",
-          1,
-          { y: "100%" },
-          { y: "0%", ease: "power2.inOut" }
-        );
       },
       beforeLeave() {
         controller.destroy();
@@ -181,6 +174,13 @@ barba.init({
           { x: "100%", stagger: 0.25, onComplete: done }
         );
         tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+        tl.fromTo(
+          ".nav-header",
+          1,
+          { y: "-100%" },
+          { y: "0%", ease: "power2.inOut" },
+          "-=1.5"
+        );
       },
     },
   ],
